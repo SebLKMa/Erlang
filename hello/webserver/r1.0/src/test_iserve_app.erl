45b409b0dd23af8991e96b68d01db49a9003eac6
d-process <user@users-MacBook-Pro.local>
2016-05-29 17:36:54 +0800

    web server sample

---
+++ hello/webserver/r1.0/src/test_iserve_app.erl
@@ -0,0 +1,16 @@
+-module(test_iserve_app).
+-export([do_get/2]).
+-include("iserve.hrl").
+ 
+do_get(#req{} = Req, Args) ->
+    {200, [], <<"<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">
+<html>
+<head>
+  <title>Welcome to iserve</title>
+</head>
+<body>
+  Hello
+</body>
+</html>">>}.
+
+
